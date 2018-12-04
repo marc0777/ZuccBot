28a4df7e724a3d4cd6f3d971ac7c7ff19f6a4d3b
Marco Fincato <marcofincato777@gmail.com>
2018-12-04 23:13:50 +0100

    Added some comments

--- src/main/java/db/Database.java
+++ src/main/java/db/Database.java
@@ -4,18 +4,35 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+/**
+ * Class responsible for the creation and connection to the database.
+ * Single tables can then be accessed by using the Connection created by this class.
+ */
 public class Database {
     private static Connection singleton;
 
+    /**
+     * Singleton for connecting to db.
+     * If a Connection has already been made it just returns it,
+     * otherwise it tries to connect.
+     *
+     * @return Connection to the db
+     */
     public static Connection getInstance() {
         if (singleton == null) connect();
         return singleton;
     }
 
+    /**
+     * Connects this class to the db instance.
+     */
     private static void connect() {
+        //TODO check if the database already exists and eventually create it
+        //DB type is SQLite
+        //DB location is "bot.db" in the project root folder
         try {
             String url = "jdbc:sqlite:bot.db";
-            singleton = DriverManager.getConnection(url);
+            singleton = DriverManager.getConnection(url); //DB connection
             System.out.println("Connection to database has been established.");
         } catch (SQLException e) {
             System.out.println(e.getMessage());
--- src/main/java/zuccante/Post.java
+++ src/main/java/zuccante/Post.java
@@ -4,13 +4,27 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+/**
+ * Representation for a single Zuccante's post.
+ */
 public class Post {
-    private final long id;
+    private final long id; // This value defaults to -1 and it's normally given by the DBMS
     private final String title;
     private final String description;
     private final String link;
-    private List<String> attachments;
+    private List<String> attachments; // Link to each attachment, if any
 
+    /**
+     * Constructor to be used when retrieving data from the db.
+     * Every parameter has to be set at construction time.
+     * Attachments needs to be deserialized.
+     *
+     * @param id          DBMS given post id
+     * @param title       post's title
+     * @param description post's description
+     * @param link        post's url
+     * @param attachments serialized attachments
+     */
     public Post(long id, String title, String description, String link, String attachments) {
         this.title = title;
         this.description = description;
@@ -19,16 +33,19 @@ public class Post {
         deserializeAttachments(attachments);
     }
 
+    /**
+     * Constructor to be used when first creating the post.
+     * The id is set to anon significant value, and attachments is initialized empty.
+     * @param title post's title
+     * @param description post's description
+     * @param link post's url
+     */
     public Post(String title, String description, String link) {
         this.title = title;
         this.description = description;
         this.link = link;
         attachments = new ArrayList<>();
         id = -1;
-    }
-
-    public void addAttachment(String attachment) {
-        attachments.add(attachment);
     }
 
     public String getTitle() {
@@ -47,17 +64,29 @@ public class Post {
         return attachments;
     }
 
+    public long getId() {
+        return id;
+    }
+
+    public void addAttachment(String attachment) {
+        attachments.add(attachment);
+    }
+
+    /**
+     * Serializes the post's attachments.
+     * @return String containing every attachment's url, separated by ';'
+     */
     public String serializedAttachments() {
         StringBuilder out = new StringBuilder();
         for (String link : attachments) out.append(link).append(";");
         return out.toString();
     }
 
+    /**
+     * Deserializes post's attachments from a given String.
+     * @param serialized String containing every attachment's url, separated by ';'
+     */
     private void deserializeAttachments(String serialized) {
         attachments = Arrays.asList(serialized.split(";"));
-    }
-
-    public long getId() {
-        return id;
     }
 }
