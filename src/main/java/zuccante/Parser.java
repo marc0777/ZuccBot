edd0781a726ff23056c7827158009478363dc498
Vitolus <30371474+Vitolus@users.noreply.github.com>
2018-12-10 14:32:16 +0100

    Update Parser.java

--- src/main/java/zuccante/Parser.java
+++ src/main/java/zuccante/Parser.java
@@ -15,47 +15,31 @@ import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Parser implements Runnable {
+public class Parser {
+    private static final String LINK = "link", ITEM = "item";
 
-    //TODO avoid eventual resource sharing problems.
-    @Override
-    public void run() {
-        System.out.println("Parser: Started.");
-        try {
-            Parser.updatePosts(Parser.readFeed("https://www.itiszuccante.gov.it/rss.xml"));
-        } catch (IOException e) {
-            System.err.println("Parser: An exception has been caught while trying to retrieve the RSS feed...");
-            e.printStackTrace();
-        } catch (XMLStreamException e) {
-            System.err.println("Parser: An exception has been caught while trying to decode the RSS feed...");
-            e.printStackTrace();
-        } finally {
-            System.out.println("Parser: Finished.");
-        }
-    }
-
-    private static final String LINK = "link";
-    private static final String ITEM = "item";
-
-    public static List<String> readFeed(String url) throws XMLStreamException, IOException {
+    public static List<String> readFeed(String url)  throws XMLStreamException, IOException{
         List<String> feed = new ArrayList<>();
         XMLEventReader eventReader = XMLInputFactory.newInstance().createXMLEventReader(new URL(url).openStream());
         String link = "";
+        XMLEvent event;
 
         while (eventReader.hasNext()) {
-            XMLEvent event = eventReader.nextEvent();
+            event = eventReader.nextEvent();
             if (event.isStartElement() && event.asStartElement().getName().getLocalPart().equals(LINK)) {
                 event = eventReader.nextEvent();
-                if (event instanceof Characters) link = event.asCharacters().getData();
+
+                if (event instanceof Characters) {
+                    link = event.asCharacters().getData();
+                }
             } else if (event.isEndElement() && event.asEndElement().getName().getLocalPart().equals(ITEM)) {
                 feed.add(link);
             }
         }
-
         return feed;
     }
 
-    public static void updatePosts(List<String> feed) throws IOException {
+    public static void updatePosts(List<String> feed) throws IOException{
         PostsDB postsDB = PostsDB.getInstance();
 
         for (String link : feed) {
@@ -64,12 +48,15 @@ public class Parser implements Runnable {
                 Elements files = document.select("span.file");
                 String description = document.select("div.field.field-name-body.field-type-text-with-summary.field-label-hidden").get(0).text();
                 String title = document.select("h1#page-title").get(0).text();
-
                 Post post = new Post(title, description, link);
-                for (Element file : files) post.addAttachment(file.child(1).attr("href"));
+
+                for (Element file : files) {
+                    post.addAttachment(file.child(1).attr("href"));
+                }
                 postsDB.addPost(post);
-                System.out.println("Parser: Added: " + title);
+                System.out.println("Added: " + title);
             }
         }
     }
+
 }
