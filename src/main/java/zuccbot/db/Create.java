ca9897ff9bcce919a76648c84b8297ab16b930e0
NekoAndLoli <vx1245399319@gmail.com>
2019-04-30 14:49:25 +0200

    Fix

--- src/main/java/zuccbot/db/Create.java
+++ src/main/java/zuccbot/db/Create.java
@@ -70,7 +70,7 @@ public class Create {
                     "\tID\tINTEGER NOT NULL UNIQUE,\n" +
                     "\tType\tTEXT NOT NULL,\n" +
                     "\tClass\tTEXT NOT NULL CHECK(Class >= 1 and Class < 6),\n" +
-                    "\tDate\tTEXT NOT NULL\n" +
+                    "\tDate\tINTEGER NOT NULL\n" +
                     ");",
             "CREATE TABLE Homework (\n" +
                     "\tID\tINTEGER NOT NULL UNIQUE,\n" +
