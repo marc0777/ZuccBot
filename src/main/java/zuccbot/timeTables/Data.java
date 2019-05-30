f9ba3ba5221c78511e32fdb77281e035348a5190
Marco Fincato <marcofincato777@gmail.com>
2019-05-30 10:26:51 +0200

    Removed commented code

--- src/main/java/zuccbot/timeTables/Data.java
+++ src/main/java/zuccbot/timeTables/Data.java
@@ -1,10 +1,6 @@
 package zuccbot.timeTables;
 
 class Data {
-    /*private float top;
-        private float left;
-        private float width;
-        private float height;*/
     private String text;
 
     public String getText() {
