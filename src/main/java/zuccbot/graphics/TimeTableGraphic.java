abda34f3ed34382cf3e3ec1a0f9d50a745be78aa
GiovanniBarozzi <34336067+GiovanniBarozzi@users.noreply.github.com>
2019-05-31 10:13:14 +0200

    Changed the time table font size

--- src/main/java/zuccbot/graphics/TimeTableGraphic.java
+++ src/main/java/zuccbot/graphics/TimeTableGraphic.java
@@ -45,7 +45,7 @@ public class TimeTableGraphic {
         for (int i = 0; i < 6; i++) {
             g2d.setFont(g2d.getFont().deriveFont(18.F));
             g2d.drawString(fLiine[i], (columnWidth * i) + 10, cellHeight - 25);
-            g2d.setFont(g2d.getFont().deriveFont(13.F));
+            g2d.setFont(g2d.getFont().deriveFont(12.F));
             for (int j = 0; j < 6; j++) {
                 if (input[i][j] != null) {
                     if (input[i][j].getSubject().length() < 20) {
